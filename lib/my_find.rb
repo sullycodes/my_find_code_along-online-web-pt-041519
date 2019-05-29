eae7c1fc05191f963615490e641af4663429b5f3
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-29 01:58:21 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,11 +3,16 @@ require 'pry'
 def my_find(collection)
   i = 0 
   while i < collection.length 
-    yield(collection[i])
-  i += 1
+  # yeild passes the element to the block outside as 
+  # part of the my_find call. which ever number returns true
+  # first will be returned by the method - which is just what
+  # .find does
+    if yield(collection[i])
+      return collection[i]
+    end
+    i += 1
   end
-  return collection
 end
 
 collection = (1..20).to_a
-my_find(collection) { |num| num.odd? }
+my_find(collection) { |num| num.even? }
